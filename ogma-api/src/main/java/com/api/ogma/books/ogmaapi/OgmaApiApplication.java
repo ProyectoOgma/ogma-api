100890845bf0f0282ad5816a81b645af8fd54577
Tomas Muzzillo <tomasmuzzillo@Laptop-de-Tomas.fibertel.com.ar>
2024-06-05 18:59:18 -0300

    initial commit

---
+++ ogma-api/src/main/java/com/api/ogma/books/ogmaapi/OgmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.ogma.books.ogmaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgmaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgmaApiApplication.class, args);
+    }
+
+}
