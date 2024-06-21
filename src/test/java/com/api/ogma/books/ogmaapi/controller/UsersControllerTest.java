d75f4cfaa01abe1523150b966f047e827e6aa1f9
tmuzzillo <tomimuzzillo@gmail.com>
2024-06-21 17:14:39 -0300

    fix errors

--- src/test/java/com/api/ogma/books/ogmaapi/controller/UsersControllerTest.java
+++ src/test/java/com/api/ogma/books/ogmaapi/controller/UsersControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
