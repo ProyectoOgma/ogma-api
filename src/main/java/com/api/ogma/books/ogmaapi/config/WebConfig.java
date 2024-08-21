5f5b35a92d39ed2e15a44849bfe30a013681c05f
tmuzzillo <tomimuzzillo@gmail.com>
2024-08-21 19:03:26 -0300

    enable CORS

---
+++ src/main/java/com/api/ogma/books/ogmaapi/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.api.ogma.books.ogmaapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile({"prod", "local"})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todos los endpoints
+                .allowedOrigins("*") // Permite cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los headers
+                .allowCredentials(true) // Habilita el envío de credenciales (cookies, headers autorizados, etc.)
+                .maxAge(3600); // Tiempo en segundos que se cachea la configuración
+    }
+}
