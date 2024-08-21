d30b89109c2972a22cde05fa02aef9022d60d0e5
tmuzzillo <tomimuzzillo@gmail.com>
2024-08-21 19:20:58 -0300

    fix cors

--- src/main/java/com/api/ogma/books/ogmaapi/config/WebConfig.java
+++ src/main/java/com/api/ogma/books/ogmaapi/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOrigins("*") // Permite cualquier origen
+                .allowedOriginPatterns("http://localhost:[*]") // Permite cualquier origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos los headers
                 .allowCredentials(true) // Habilita el envío de credenciales (cookies, headers autorizados, etc.)
