46f676273f6b953a80629bf29920cc0d13086e6c
Juan Cruz marquez <juan.marquez@vemo.global>
2024-08-31 19:00:10 -0300

    fix returning post entity

--- src/main/java/com/api/ogma/books/ogmaapi/adapter/handler/PostHandler.java
+++ src/main/java/com/api/ogma/books/ogmaapi/adapter/handler/PostHandler.java
@@ -2,11 +2,15 @@ package com.api.ogma.books.ogmaapi.adapter.handler;
 
 import com.api.ogma.books.ogmaapi.dto.domain.PostDTO;
 import com.api.ogma.books.ogmaapi.dto.request.PostRequest;
+import com.api.ogma.books.ogmaapi.model.Post;
 import com.api.ogma.books.ogmaapi.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +19,25 @@ public class PostHandler {
     private final ObjectMapper objectMapper;
     private final PostService postService;
 
-    public void createPost(PostRequest postRequest) {
+    public PostDTO createPost(PostRequest postRequest) {
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         PostDTO postDTO = objectMapper.convertValue(postRequest, PostDTO.class);
-        postService.createPost(postDTO);
+        Post post = postService.createPost(postDTO);
+        return objectMapper.convertValue(post, PostDTO.class);
+    }
+
+    public PostDTO getPost(Long id) {
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        Post post = postService.getPost(id);
+        return objectMapper.convertValue(post, PostDTO.class);
+    }
+
+    public List<PostDTO> getAllPosts() {
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        List<Post> posts = postService.getAllPosts();
+        return posts.stream()
+                .map(post -> objectMapper.convertValue(post, PostDTO.class))
+                .collect(Collectors.toList());
     }
 
 }
