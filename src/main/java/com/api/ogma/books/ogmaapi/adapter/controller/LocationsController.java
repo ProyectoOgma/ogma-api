fb582f85002e6ca61de4a988514cc9404aeb7c07
Juan Cruz marquez <juan.marquez@vemo.global>
2024-08-31 19:00:59 -0300

    refactor return with response interface

--- src/main/java/com/api/ogma/books/ogmaapi/adapter/controller/LocationsController.java
+++ src/main/java/com/api/ogma/books/ogmaapi/adapter/controller/LocationsController.java
@@ -4,10 +4,15 @@ import com.api.ogma.books.ogmaapi.adapter.handler.MunicipalityHandler;
 import com.api.ogma.books.ogmaapi.adapter.handler.ProvinceHandler;
 import com.api.ogma.books.ogmaapi.dto.domain.MunicipalityDTO;
 import com.api.ogma.books.ogmaapi.dto.domain.ProvinceDTO;
+import com.api.ogma.books.ogmaapi.dto.response.Response;
+import com.api.ogma.books.ogmaapi.dto.response.ResponseUtil;
 import com.api.ogma.books.ogmaapi.model.Municipality;
 import com.api.ogma.books.ogmaapi.model.Province;
 import com.api.ogma.books.ogmaapi.service.MunicipalityService;
 import com.api.ogma.books.ogmaapi.service.ProvinceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,14 +31,37 @@ public class LocationsController {
     private final ProvinceHandler provinceHandler;
     private final MunicipalityHandler municipalityHandler;
 
+    @Operation(summary = "Get all provinces")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Provincias encontradas"),
+            @ApiResponse(responseCode = "500", description = "Error al obtener las provincias, intentelo nuevamente")
+    })
     @GetMapping("/provinces")
-    public ResponseEntity<List<ProvinceDTO>> getAllProvinces() {
-        return new ResponseEntity<>(provinceHandler.getProvincesForm(), HttpStatus.OK);
+    public ResponseEntity<Response<List<ProvinceDTO>>> getAllProvinces() {
+        try {
+            List<ProvinceDTO> provinces = provinceHandler.getProvincesForm();
+            String message = provinces == null ? "Error al obtener las provincias, intentelo nuevamente" : "Provincias encontradas";
+
+            return ResponseUtil.createSuccessResponse(provinces, message);
+        }catch (Exception e) {
+            return ResponseUtil.createErrorResponse("Error al obtener las provincias, intentelo nuevamente", HttpStatus.INTERNAL_SERVER_ERROR, List.of(e.getMessage()));
+        }
     }
 
+    @Operation(summary = "Get all municipalities by province")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Municipios encontrados"),
+            @ApiResponse(responseCode = "500", description = "Error al obtener los municipios, intentelo nuevamente")
+    })
     @GetMapping("/municipalities")
-    public ResponseEntity<List<MunicipalityDTO>> getMunicipalitiesByProvince(@RequestParam Long provinceId) {
-        List<MunicipalityDTO> municipalities = municipalityHandler.getMunicipalitiesForm(provinceId);
-        return new ResponseEntity<>(municipalities, HttpStatus.OK);
+    public ResponseEntity<Response<List<MunicipalityDTO>>> getMunicipalitiesByProvince(@RequestParam Long provinceId) {
+        try {
+            List<MunicipalityDTO> municipalities = municipalityHandler.getMunicipalitiesForm(provinceId);
+            String message = municipalities == null ? "Error al obtener los municipios, intentelo nuevamente" : "Municipios encontrados";
+
+            return ResponseUtil.createSuccessResponse(municipalities, message);
+        } catch (Exception e) {
+            return ResponseUtil.createErrorResponse("Error al obtener los municipios, intentelo nuevamente", HttpStatus.INTERNAL_SERVER_ERROR, List.of(e.getMessage()));
+        }
     }
 }
