9f7017227996792ce6c911cb6146501bf1c9c334
tmuzzillo <tomimuzzillo@gmail.com>
2024-09-21 12:48:25 -0300

    change email template

--- src/main/java/com/api/ogma/books/ogmaapi/common/factory/EmailTemplateFactory.java
+++ src/main/java/com/api/ogma/books/ogmaapi/common/factory/EmailTemplateFactory.java
@@ -2,6 +2,7 @@ package com.api.ogma.books.ogmaapi.common.factory;
 
 import com.api.ogma.books.ogmaapi.model.ExchangeOffer;
 import com.api.ogma.books.ogmaapi.model.Post;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,8 +13,8 @@ public class EmailTemplateFactory {
         Map<String, Object> templateModel = new HashMap<>();
         Post post = exchangeOffer.getPost();
         Post postOffered = exchangeOffer.getOfferedPost();
-        templateModel.put("userName", post.getUser().getUserSiteName());
-        templateModel.put("offerUser", postOffered.getUser().getUserSiteName());
+        templateModel.put("userName", StringUtils.capitalize(post.getUser().getUserSiteName()));
+        templateModel.put("offerUser", StringUtils.capitalize(postOffered.getUser().getUserSiteName()));
         templateModel.put("offeredBook", postOffered.getBook().getTitle());
         templateModel.put("requestedBook", post.getBook().getTitle());
         templateModel.put("offerId", exchangeOffer.getId());
