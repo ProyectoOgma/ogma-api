75462fbd2b90a61c982bc382b1e4a7e61d9a6a47
Juan Cruz Marquez <juan.marquez@vemo.global>
2024-10-23 20:24:50 -0300

    added properties to update and genre in get response

--- src/main/java/com/api/ogma/books/ogmaapi/dto/domain/UserDTO.java
+++ src/main/java/com/api/ogma/books/ogmaapi/dto/domain/UserDTO.java
@@ -1,13 +1,18 @@
 package com.api.ogma.books.ogmaapi.dto.domain;
 
 import com.api.ogma.books.ogmaapi.model.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Builder
@@ -26,7 +31,8 @@ public class UserDTO {
         private String token;
         @JsonProperty("userGenre")
         private String genre;
-        private Date birthDate;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate birthDate;
         @JsonProperty("userLocation")
         private UserLocationDTO userLocationDTO;
         private String address;
