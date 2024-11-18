1c9605d6ef43e70756b4f3256d5581a8a0a82508
Tomas Muzzillo <71854664+tmuzzillo@users.noreply.github.com>
2024-11-18 16:03:52 -0300

    Feature/fix url mails (#30)
    * fix url email offer template
    
    * redireccion para confirmacion de datos

--- src/main/java/com/api/ogma/books/ogmaapi/common/factory/EmailTemplateFactory.java
+++ src/main/java/com/api/ogma/books/ogmaapi/common/factory/EmailTemplateFactory.java
@@ -19,7 +19,7 @@ public class EmailTemplateFactory {
         templateModel.put("offerUser", StringUtils.capitalize(postOffered.getUser().getUserSiteName()));
         templateModel.put("offeredBook", postOffered.getBook().getTitle());
         templateModel.put("requestedBook", post.getBook().getTitle());
-        templateModel.put("offerId", exchangeOffer.getId());
+        templateModel.put("postId", post.getId());
         return templateModel;
     }
 
@@ -27,6 +27,7 @@ public class EmailTemplateFactory {
         Map<String, Object> templateModel = new HashMap<>();
         templateModel.put("userName", StringUtils.capitalize(exchange.getExchangeOffer().getOfferedPost().getUser().getUserSiteName()));
         templateModel.put("acceptedUser", StringUtils.capitalize(exchange.getExchangeOffer().getPost().getUser().getUserSiteName()));
+        templateModel.put("exchangeId", exchange.getId());
         return templateModel;
     }
 
