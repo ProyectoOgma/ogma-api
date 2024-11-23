2bf0e8740baa358eff64539c6249fd277277530e
Juan Cruz marquez <juan.marquez@vemo.global>
2024-11-23 12:16:20 -0300

    add new data and seed

--- src/main/java/com/api/ogma/books/ogmaapi/common/Seeder.java
+++ src/main/java/com/api/ogma/books/ogmaapi/common/Seeder.java
@@ -45,6 +45,18 @@ public class Seeder implements CommandLineRunner {
                 .role(Role.USER)
                 .build();
 
+        User updateJulianUser = userRepository.findByEmail("julianismael13@gmail.com").orElse(null);
+        assert updateJulianUser != null;
+        updateJulianUser.setHashedPassword(passwordEncoder.encode("password"));
+        userRepository.save(updateJulianUser);
+        System.out.println("User seeded: " + updateJulianUser);
+
+        User updateAylenUser = userRepository.findByEmail("aylenrominapattuzzi@gmail.com").orElse(null);
+        assert updateAylenUser != null;
+        updateAylenUser.setHashedPassword(passwordEncoder.encode("password"));
+        userRepository.save(updateAylenUser);
+        System.out.println("User seeded: " + updateAylenUser);
+
         User userSaved = userRepository.save(user);
         System.out.println("User seeded: " + userSaved);
     }
