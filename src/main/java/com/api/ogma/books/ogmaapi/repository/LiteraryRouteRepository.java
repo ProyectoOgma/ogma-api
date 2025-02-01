33b3f9dd240042843d819b8317851d9bb0893aee
Tomas Muzzillo <71854664+tmuzzillo@users.noreply.github.com>
2025-02-01 14:48:13 -0300

    Obtener las rutas literarias creadas por el usuario logueado (#37)

--- src/main/java/com/api/ogma/books/ogmaapi/repository/LiteraryRouteRepository.java
+++ src/main/java/com/api/ogma/books/ogmaapi/repository/LiteraryRouteRepository.java
@@ -1,7 +1,12 @@
 package com.api.ogma.books.ogmaapi.repository;
 
 import com.api.ogma.books.ogmaapi.model.LiteraryRoute;
+import com.api.ogma.books.ogmaapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LiteraryRouteRepository extends JpaRepository<LiteraryRoute, Long> {
+
+    List<LiteraryRoute> findAllByUser(User user);
 }
