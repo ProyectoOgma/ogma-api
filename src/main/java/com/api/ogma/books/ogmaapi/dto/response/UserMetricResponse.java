5c0a0c505240aa9570dd1c97e79f5b39625c25a6
tmuzzillo <tomimuzzillo@gmail.com>
2025-07-14 21:14:13 -0300

    return snake case metrics

--- src/main/java/com/api/ogma/books/ogmaapi/dto/response/UserMetricResponse.java
+++ src/main/java/com/api/ogma/books/ogmaapi/dto/response/UserMetricResponse.java
@@ -1,10 +1,13 @@
 package com.api.ogma.books.ogmaapi.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserMetricResponse {
     private Integer Sells;
     private Integer Exchanges;
