2301e28792e202383b699cf8d6b6c02a401730a7
tmuzzillo <tomimuzzillo@gmail.com>
2025-07-30 18:57:21 -0300

    add cors for front AWS

--- src/main/java/com/api/ogma/books/ogmaapi/config/WebConfig.java
+++ src/main/java/com/api/ogma/books/ogmaapi/config/WebConfig.java
@@ -12,7 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOriginPatterns("*","http://181.93.93.222:[*]", "181.93.93.222", "http://localhost:[*]")
+                .allowedOriginPatterns("*",
+                        "http://181.93.93.222:[*]",
+                        "181.93.93.222",
+                        "http://localhost:[*]",
+                        "https://main.d34n0i2687403.amplifyapp.com",
+                        "http://www.ogma.com.ar",
+                        "https://www.ogma.com.ar")
                 .allowedMethods("*") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos los headers
                 .allowCredentials(true) // Habilita el envío de credenciales (cookies, headers autorizados, etc.)
